9dc00eb0e71b1f385f1a59e384423ff4956c1545
baskar44 <baskg99@gmail.com>
2018-08-04 21:03:48 +1000

    Added text fields for title, tags and content

--- simple-blogger-app/src/components/posts_new.js
+++ simple-blogger-app/src/components/posts_new.js
@@ -2,11 +2,39 @@ import React, {Component} from 'react'
 import {Field, reduxForm} from 'redux-form'
 
 class PostsNew extends Component {
+
+    renderField(field){
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div>
-                <h1>Add Post</h1>
-            </div>
+            <form className="container">
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         )
     }
 }
