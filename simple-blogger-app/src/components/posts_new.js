7ba8ef1e05bd7838af83a13735d39e1a48e61e57
baskar44 <baskg99@gmail.com>
2018-08-06 15:58:48 +1000

    Added add-post feature

--- simple-blogger-app/src/components/posts_new.js
+++ simple-blogger-app/src/components/posts_new.js
@@ -1,24 +1,43 @@
 import React, {Component} from 'react'
 import {Field, reduxForm} from 'redux-form'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {createPost} from '../actions'
+
 
 class PostsNew extends Component {
 
     renderField(field){
         return (
-            <div className="form-group">
+            <div className="form-group was-validated">
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                    {field.meta.touched ? field.meta.error : ''}
+                </div>
+                
             </div>
         )
     }
 
+    onSubmit(values){
+        this.props.createPost(values, () => {
+            this.props.history.push('/')
+        })
+    }
+
     render() {
+
+        const { handleSubmit } = this.props
+
         return (
-            <form className="container">
+            <form 
+                onSubmit={handleSubmit(this.onSubmit.bind(this))} 
+                className="container">
                 <Field
                     label="Title"
                     name="title"
@@ -26,7 +45,7 @@ class PostsNew extends Component {
                 />
                 <Field
                     label="Tags"
-                    name="tags"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -34,11 +53,38 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="action btn btn-success">Submit</button>
+                <Link to="/" className="action btn btn-danger">Cancel</Link>
+
             </form>
         )
     }
 }
 
+
+function validate(values){
+    const errors = {}
+
+    if (!values.title) {
+        errors.title = "Enter a title"
+    }
+
+    if (!values.tags) {
+        errors.tags = "Enter at least one tag"
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content"
+    }
+
+    return errors
+
+}
+
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, {createPost}) (PostsNew)
+);
